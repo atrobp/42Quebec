565b1f6dfc76b3bcdace4650aa271b7ba51c261d
Arber Topalli <atopalli@c1r3p8.42quebec.com>
2023-02-22 17:55:42 -0500

    update

--- philosopher/ft_main.c
+++ philosopher/ft_main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_main.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: atopalli <atopalli@42.quebec.com>          +#+  +:+       +#+        */
+/*   By: atopalli <atopalli@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/04 14:47:50 by atopalli          #+#    #+#             */
-/*   Updated: 2023/02/15 13:07:53 by atopalli         ###   ########.fr       */
+/*   Updated: 2023/02/22 13:23:00 by atopalli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,49 +20,69 @@ int	main(int ac, char **av)
 	i = 0;
 	if (ac >= 5 && ac <= 6)
 	{
-		while (++i < ac)
+		p = ft_philo_check(ac, av);
+		if (p.valid == true)
 		{
-			if (ft_atoi(av[i]) <= 0)
-				return (0);
+			ft_philo_init(&p);
 		}
-		p.t_must_eat = -1;
-		p.n_philo = ft_atoi(av[1]);
-		p.tid = malloc(sizeof(p.tid) * p.n_philo);
-		p.phork = malloc(sizeof(p.phork) * p.n_philo);
-		p.tt_die_index = malloc(sizeof(int) * p.n_philo);
-		p.t2die = ft_atoi(av[2]);
-		p.t2eat = ft_atoi(av[3]);
-		p.t2sleep = ft_atoi(av[4]);
-		if (ac == 6)
-			p.t_must_eat = ft_atoi(av[5]);
-		ft_philo(&p);
 	}
 	return (0);
 
 }
 
-void	ft_philo(t_list *p)
+void	ft_philo_init(t_list *p)
 {
 	int	i;
 
-	i = 0;
-	while (i < p->n_philo)
+	i = -1;
+	while (++i < p->n_philo)
 	{
 		pthread_mutex_init(&p->phork[i], NULL);
-		i += 1;
 	}
-	i = 0;
-	while (i < p->n_philo)
+	i = -1;
+	while (++i < p->n_philo)
 	{
-		pthread_create(&p->tid[i], NULL, ft_philo, (void *)p);
-		i += 1;
+		pthread_create(&p->tid[i], NULL, ft_philosopher, (void *)p);
 	}
-	i = 0;
-	while (i < p->n_philo)
+	i = -1;
+	while (++i < p->n_philo)
 	{
 		pthread_join(p->tid[i], NULL);
-		i += 1;
 	}
+	i = -1;
+	while (++i < p->n_philo)
+	{
+		pthread_mutex_destroy(&p->phork[i]);
+	}
+}
+
+t_list	ft_philo_check(int ac, char **av)
+{
+	t_list	p;
+	int		i;
+
+	i = 0;
+	while (++i < ac)
+	{
+		if (ft_atoi(av[i]) <= 0)
+		{
+			p.valid = false;
+			return (p);
+		}
+	}
+	i = 0;
+	p.t_must_eat = -1;
+	p.n_philo = ft_atoi(av[1]);
+	p.phork = malloc(sizeof(p.phork) * p.n_philo);
+	p.tid = malloc(sizeof(p.tid) * p.n_philo);
+	p.tt_die_index = malloc(sizeof(int) * p.n_philo);
+	p.t2die = ft_atoi(av[2]);
+	p.t2eat = ft_atoi(av[3]);
+	p.t2sleep = ft_atoi(av[4]);
+	if (ac == 6)
+		p.t_must_eat = ft_atoi(av[5]);
+	p.valid = true;
+	return (p);
 }
 
 int	ft_atoi(char *str)
--- philosopher/ft_philo.h
+++ philosopher/ft_philo.h
@@ -3,15 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   ft_philo.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: atopalli <atopalli@42.quebec.com>          +#+  +:+       +#+        */
+/*   By: atopalli <atopalli@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/04 14:48:56 by atopalli          #+#    #+#             */
-/*   Updated: 2023/02/15 12:58:22 by atopalli         ###   ########.fr       */
+/*   Updated: 2023/02/22 13:04:26 by atopalli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PHILO_H
-# define FT_PHIL_H
+# define FT_PHILO_H
 
 # include <pthread.h>
 # include <stdbool.h>
@@ -31,10 +31,12 @@ typedef struct s_list
 	int				t2eat;
 	int				t2sleep;
 	int				t_must_eat;
+	bool			valid;
 }	t_list;
 
 int		ft_atoi(char *str);
-void	ft_philo(t_list *p);
-
+void	ft_philo_init(t_list *p);
+t_list	ft_philo_check(int ac, char **av);
+void	*ft_philosopher(void *arg);
 
 #endif
