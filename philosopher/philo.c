0e55c9e80d473ce2d7f2c7768225adb70ee5ff04
Arber Topalli <atopalli@c1r3p8.42quebec.com>
2023-03-06 13:05:53 -0500

    update almost done

--- philosopher/philo.c
+++ philosopher/philo.c
@@ -3,10 +3,10 @@
 /*                                                  if(success){};            */
 /*   philo.c                                        ██╗  ██╗██████╗           */
 /*                                                  ██║  ██║╚════██╗          */
-/*   By: atopalli atopalli@student.42quebec.com     ███████║ █████╔╝          */
+/*   By: atopalli | github/atrobp                   ███████║ █████╔╝          */
 /*                                                  ╚════██║██╔═══╝           */
 /*   Created: 2023/03/05 20:52:20 by atopalli            ██║███████╗          */
-/*   Updated: 2023/03/05 23:06:23 by atopalli            ╚═╝╚══════╝.qc       */
+/*   Updated: 2023/03/06 11:21:46 by atopalli            ╚═╝╚══════╝.qc       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,21 @@ void	*ft_philo(void *arg)
 	t_philo	*philo;
 
 	philo = (t_philo *)arg;
+	philo->last_eat = ft_gettime();
+	if (philo->id % 2 == 0)
+		ft_usleep(100);
 	while (1)
 	{
 		ft_eat(philo);
-		ft_sleep(philo);
-		ft_print(philo, "is thinking");
+		ft_think(philo);
+		if (philo->last_eat - ft_gettime() > philo->data->t_die)
+		{
+			ft_print(philo, "died");
+			return (NULL);
+		}
+		return (NULL);
 	}
-	return (NULL);
 }
-
 void	ft_eat(t_philo *philo)
 {
 	pthread_mutex_lock(&philo->left_fork);
@@ -33,11 +39,12 @@ void	ft_eat(t_philo *philo)
 	pthread_mutex_lock(philo->right_fork);
 	ft_print(philo, "has taken a fork");
 	ft_print(philo, "is eating");
+	ft_usleep(philo->data->t_eat);
+	philo->n_eat++;
 	pthread_mutex_unlock(&philo->left_fork);
 	pthread_mutex_unlock(philo->right_fork);
 	philo->last_eat = ft_gettime();
-	ft_usleep(philo->data->t_eat);
-	philo->n_eat++;
+	ft_sleep(philo);
 }
 
 void	ft_sleep(t_philo *philo)
@@ -49,7 +56,6 @@ void	ft_sleep(t_philo *philo)
 void	ft_think(t_philo *philo)
 {
 	ft_print(philo, "is thinking");
-	ft_usleep(philo->data->t_sleep);
 }
 
 void	ft_usleep(unsigned int time)
